d86098a95ff17e808436c6c81b10b9e39b4c9ca0
Toluwanimi Agboola <toluwanimiagboola@gmail.com>
2022-10-05 22:18:55 +0100

    Add charts to E-Commerce page, load table in Orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div className="m-2 md:m-10 p-1 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent id="gridComp" dataSource={ordersData}>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Orders;
