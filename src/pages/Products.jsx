d09d8ee2800d738fbd24153ae766e793bbfe0868
Toluwanimi Agboola <toluwanimiagboola@gmail.com>
2022-10-08 11:03:18 +0100

    Update styles on products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,17 +7,25 @@ function Products() {
     <div className="m-2 md:m-10 mt-24 p-6 md:p-10 bg-white dark:bg-[#42464D] rounded-3xl">
       <ChartsHeader category="Page" title="Products" />
       <div className="flex flex-wrap justify-center md:justify-between">
-        {ordersData.map((item, index) => (
-          <div className="drop-shadow-xl flex flex-col p-2 m-4 w-48 md:w-32">
-            <img
-              src={item.ProductImage}
-              alt="Person"
-              className="rounded-2xl w-40 h-40 md:w-28 md:h-28"
-            />
-            <p className="font-bold mt-2 text-lg">{item.OrderItems}</p>
-            <p>${item.TotalAmount}</p>
-          </div>
-        ))}
+        {ordersData.map(
+          (item, index) =>
+            index <= 24 && (
+              <div
+                key={index}
+                className="shadow-lg rounded-2xl flex flex-col p-4 m-4 w-48 md:w-38"
+              >
+                <img
+                  src={item.ProductImage}
+                  alt="Person"
+                  className="rounded-2xl w-40 h-40 md:w-38 md:h-38"
+                />
+                <p className="font-bold mt-2 text-gray-800">
+                  {item.OrderItems}
+                </p>
+                <p className="text-gray-600">${item.TotalAmount}</p>
+              </div>
+            )
+        )}
       </div>
     </div>
   );
