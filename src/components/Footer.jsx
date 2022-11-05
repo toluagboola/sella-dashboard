67ed58590d08d0d97bc51b02ed5af61f749dd861
toluagboola <toluwanimiagboola@gmail.com>
2022-11-05 10:47:47 +0100

    Update footer text colors in dark mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   const { currentColor } = useStateContext();
   return (
     <div className="p-10 mt-8 flex flex-col items-center justify-center">
-      <p className="mb-2 text-lg">
+      <p className="mb-2 text-lg dark:text-gray-400">
         Built by{" "}
         <a
           href="https://toluagboola.github.io/s"
@@ -20,20 +20,23 @@ const Footer = () => {
       <div className="flex justify-between">
         <a
           href="https://github.com/toluagboola"
-          className="text-2xl rounded-full p-3 hover:bg-gray-200 mx-1"
+          title="GitHub"
+          className="text-2xl rounded-full p-3 hover:bg-gray-200 dark:hover:bg-gray-300 dark:hover:text-gray-600 dark:text-gray-500 mx-1"
         >
           <FaGithubSquare />
         </a>
         <a
           href="https://www.linkedin.com/in/toluwanimi-agboola-025568204/"
-          className="text-2xl rounded-full p-3 hover:bg-gray-200 mx-1"
+          title="LinkedIn"
+          className="text-2xl rounded-full p-3 hover:bg-gray-200 dark:hover:bg-gray-300 dark:hover:text-gray-600 dark:text-gray-500 mx-1"
         >
           <FaLinkedin />
         </a>
 
         <a
           href="https://twitter.com/thetoluisaiah"
-          className="text-2xl rounded-full p-3 hover:bg-gray-200 mx-1"
+          title="Twitter"
+          className="text-2xl rounded-full p-3 hover:bg-gray-200 dark:hover:bg-gray-300 dark:hover:text-gray-600 dark:text-gray-500 mx-1"
         >
           <FaTwitterSquare />
         </a>
